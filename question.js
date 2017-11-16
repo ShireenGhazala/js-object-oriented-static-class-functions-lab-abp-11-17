4bdb3b077a10a1fa2b20aebcd8b185e1d54d5f7a
Shireen Ghazala <shireen.gaz@gmail.com>
2017-11-16 09:42:01 +0000

    Done.

--- question.js
+++ question.js
@@ -1,1 +1,23 @@
 'use strict';
+
+class Question{
+     constructor(content){
+      this.content = content;
+      this.save()
+     }
+
+static Find(id){
+return this.All() [id-1]
+}
+
+
+
+save(){
+return this.constructor._All.push(this)
+}
+static All(){
+return this._All;
+}
+
+}
+Question._All=[]
